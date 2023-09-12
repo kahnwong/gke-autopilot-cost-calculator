e03a1cf1ca0aeaee004cc9130d7db5405de1b8bc
kahnwong <karn@karnwong.me>
2023-09-12 11:44:16 +0700

    add spark job calculator

--- app.py
+++ app.py
@@ -1,34 +1,56 @@
 import streamlit as st
 
-from gke_autopilot_cost_calculator.main import create_chart
-from gke_autopilot_cost_calculator.main import create_dataframe
+from gke_autopilot_cost_calculator.workload_application import create_chart
+from gke_autopilot_cost_calculator.workload_application import create_dataframe
+from gke_autopilot_cost_calculator.workload_spark_job import (
+    calculate_spark_job_regular_spot_price,
+)
+from gke_autopilot_cost_calculator.workload_spark_job import (
+    calculate_spark_job_scale_out_arm_spot_price,
+)
 
 
 st.title("GKE Autopilot Cost Calculator")
 
 
-# compute_type, instance_type = st.columns(2)
-
-# with compute_type:
-#     st.radio(
-#         "Compute Type",
-#         ["General Purpose", "Scale-Out: ARM", "Scale-Out: x86"],
-#     )
-
-# with instance_type:
-#     st.radio(
-#         "Instance Type",
-#         ["Regular", "Spot"],
-#     )
-
-
 with st.sidebar:
-    cpu = st.number_input(label="cpu", min_value=0.25, max_value=8.0, step=0.25)
-    memory = st.number_input(label="memory", min_value=0.5, max_value=8.0, step=0.5)
+    # ---------- Application ---------- #
+    cpu = st.number_input(label="cpu", min_value=0.25, max_value=200.0, step=0.25)
+    memory = st.number_input(label="memory", min_value=0.5, max_value=200.0, step=0.5)
+
+    # ---------- Spark Job ---------- #
+    workload_type = st.radio(
+        "Workload Type",
+        ["Application", "Spark Jobs"],
+    )
+    if workload_type == "Spark Jobs":
+        executors = st.number_input(
+            label="Executors", min_value=1, max_value=100, step=1
+        )
+        job_duration_seconds = st.number_input(
+            label="Job Duration (Minute)", min_value=1, max_value=1000, step=1
+        )
 
 st.write("Region: asia-southeast1")
 st.write("Unit: USD")
 
-df = create_dataframe(cpu=cpu, memory=memory)
-st.dataframe(df)
-st.pyplot(create_chart(df))
+if workload_type == "Application":
+    df = create_dataframe(cpu=cpu, memory=memory)
+    st.dataframe(df)
+    st.pyplot(create_chart(df))
+elif workload_type == "Spark Jobs":
+    regular_spot_price = calculate_spark_job_regular_spot_price(
+        cpu=cpu,
+        memory=memory,
+        executors=executors,
+        job_duration_seconds=job_duration_seconds,
+    )
+    st.write(f"Regular-Spot Price: {regular_spot_price:.20f}")
+
+    scale_out_arm_spot_price = calculate_spark_job_scale_out_arm_spot_price(
+        cpu=cpu,
+        memory=memory,
+        executors=executors,
+        job_duration_seconds=job_duration_seconds,
+    )
+    st.write(f"Regular-Spot Price: {scale_out_arm_spot_price:.20f}")
---
+++ gke_autopilot_cost_calculator/workload_spark_job.py
@@ -0,0 +1,23 @@
+from .model import gke
+
+
+def calculate_spark_job_regular_spot_price(
+    cpu: float, memory: float, executors: int, job_duration_seconds: int
+) -> float:
+    price_per_node_in_hour = (gke.GKEPodGeneralPurpose().cpu_spot * cpu) + (
+        gke.GKEPodGeneralPurpose().memory_spot * memory
+    )
+    price_per_node_in_second = price_per_node_in_hour / 60 / 60 * job_duration_seconds
+
+    return price_per_node_in_second * (1 + executors)  # need to also add driver node
+
+
+def calculate_spark_job_scale_out_arm_spot_price(
+    cpu: float, memory: float, executors: int, job_duration_seconds: int
+) -> float:
+    price_per_node_in_hour = (gke.GKEPodScaleOutARM().cpu_spot * cpu) + (
+        gke.GKEPodScaleOutARM().memory_spot * memory
+    )
+    price_per_node_in_second = price_per_node_in_hour / 60 / 60 * job_duration_seconds
+
+    return price_per_node_in_second * (1 + executors)  # need to also add driver node
