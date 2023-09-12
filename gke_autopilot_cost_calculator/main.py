f55709182d2470451d516cbf6a7d5efabf622ae2
kahnwong <karn@karnwong.me>
2023-09-12 10:37:24 +0700

    fix operators bug

--- gke_autopilot_cost_calculator/main.py
+++ gke_autopilot_cost_calculator/main.py
@@ -14,76 +14,78 @@ def create_dataframe(cpu: float, memory: float) -> pd.DataFrame:
             {
                 "Compute Type": "General Purpose",
                 "Pricing": "Regular",
-                "Price/Hour": gke.GKEPodGeneralPurpose().cpu_regular * cpu
-                + gke.GKEPodGeneralPurpose().memory_regular * memory,
+                "Price/Hour": (gke.GKEPodGeneralPurpose().cpu_regular * cpu)
+                + (gke.GKEPodGeneralPurpose().memory_regular * memory),
             },
             {
                 "Compute Type": "General Purpose",
                 "Pricing": "Spot",
-                "Price/Hour": gke.GKEPodGeneralPurpose().cpu_spot * cpu
-                + gke.GKEPodGeneralPurpose().memory_spot * memory,
+                "Price/Hour": (gke.GKEPodGeneralPurpose().cpu_spot * cpu)
+                + (gke.GKEPodGeneralPurpose().memory_spot * memory),
             },
             {
                 "Compute Type": "General Purpose",
                 "Pricing": "1 Year Commitment",
-                "Price/Hour": gke.GKEPodGeneralPurpose().cpu_one_year_commitment * cpu
-                + gke.GKEPodGeneralPurpose().memory_one_year_commitment * memory,
+                "Price/Hour": (gke.GKEPodGeneralPurpose().cpu_one_year_commitment * cpu)
+                + (gke.GKEPodGeneralPurpose().memory_one_year_commitment * memory),
             },
             {
                 "Compute Type": "General Purpose",
                 "Pricing": "3 Year Commitment",
-                "Price/Hour": gke.GKEPodGeneralPurpose().cpu_three_year_commitment * cpu
-                + gke.GKEPodGeneralPurpose().memory_three_year_commitment * memory,
+                "Price/Hour": (
+                    gke.GKEPodGeneralPurpose().cpu_three_year_commitment * cpu
+                )
+                + (gke.GKEPodGeneralPurpose().memory_three_year_commitment * memory),
             },
             # ---------------------------- Scale-Out: ARM ---------------------------- #
             {
                 "Compute Type": "Scale-Out: ARM",
                 "Pricing": "Regular",
-                "Price/Hour": gke.GKEPodScaleOutARM().cpu_regular * cpu
-                + gke.GKEPodScaleOutARM().memory_regular * memory,
+                "Price/Hour": (gke.GKEPodScaleOutARM().cpu_regular * cpu)
+                + (gke.GKEPodScaleOutARM().memory_regular * memory),
             },
             {
                 "Compute Type": "Scale-Out: ARM",
                 "Pricing": "Spot",
-                "Price/Hour": gke.GKEPodScaleOutARM().cpu_spot * cpu
-                + gke.GKEPodScaleOutARM().memory_spot * memory,
+                "Price/Hour": (gke.GKEPodScaleOutARM().cpu_spot * cpu)
+                + (gke.GKEPodScaleOutARM().memory_spot * memory),
             },
             {
                 "Compute Type": "Scale-Out: ARM",
                 "Pricing": "1 Year Commitment",
-                "Price/Hour": gke.GKEPodScaleOutARM().cpu_one_year_commitment * cpu
-                + gke.GKEPodScaleOutARM().memory_one_year_commitment * memory,
+                "Price/Hour": (gke.GKEPodScaleOutARM().cpu_one_year_commitment * cpu)
+                + (gke.GKEPodScaleOutARM().memory_one_year_commitment * memory),
             },
             {
                 "Compute Type": "Scale-Out: ARM",
                 "Pricing": "3 Year Commitment",
-                "Price/Hour": gke.GKEPodScaleOutARM().cpu_three_year_commitment * cpu
-                + gke.GKEPodScaleOutARM().memory_three_year_commitment * memory,
+                "Price/Hour": (gke.GKEPodScaleOutARM().cpu_three_year_commitment * cpu)
+                + (gke.GKEPodScaleOutARM().memory_three_year_commitment * memory),
             },
             # ---------------------------- Scale-Out: x86 ---------------------------- #
             {
                 "Compute Type": "Scale-Out: x86",
                 "Pricing": "Regular",
-                "Price/Hour": gke.GKEPodScaleOutX86().cpu_regular * cpu
-                + gke.GKEPodScaleOutX86().memory_regular * memory,
+                "Price/Hour": (gke.GKEPodScaleOutX86().cpu_regular * cpu)
+                + (gke.GKEPodScaleOutX86().memory_regular * memory),
             },
             {
                 "Compute Type": "Scale-Out: x86",
                 "Pricing": "Spot",
-                "Price/Hour": gke.GKEPodScaleOutX86().cpu_spot * cpu
-                + gke.GKEPodScaleOutX86().memory_spot * memory,
+                "Price/Hour": (gke.GKEPodScaleOutX86().cpu_spot * cpu)
+                + (gke.GKEPodScaleOutX86().memory_spot * memory),
             },
             {
                 "Compute Type": "Scale-Out: x86",
                 "Pricing": "1 Year Commitment",
-                "Price/Hour": gke.GKEPodScaleOutX86().cpu_one_year_commitment * cpu
-                + gke.GKEPodScaleOutX86().memory_one_year_commitment * memory,
+                "Price/Hour": (gke.GKEPodScaleOutX86().cpu_one_year_commitment * cpu)
+                + (gke.GKEPodScaleOutX86().memory_one_year_commitment * memory),
             },
             {
                 "Compute Type": "Scale-Out: x86",
                 "Pricing": "3 Year Commitment",
-                "Price/Hour": gke.GKEPodScaleOutX86().cpu_three_year_commitment * cpu
-                + gke.GKEPodScaleOutX86().memory_three_year_commitment * memory,
+                "Price/Hour": (gke.GKEPodScaleOutX86().cpu_three_year_commitment * cpu)
+                + (gke.GKEPodScaleOutX86().memory_three_year_commitment * memory),
             },
         ]
     )
